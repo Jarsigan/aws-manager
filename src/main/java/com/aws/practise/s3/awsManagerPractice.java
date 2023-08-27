024c450f80e5b5fa73a2564e1421d35a6a91facf
Jarsigan <jarsigan.15@cse.mrt.ac.lk>
2023-08-27 20:48:10 +0800

    Initial commit for aws manager

---
+++ src/main/java/com/aws/practise/s3/awsManagerPractice.java
@@ -0,0 +1,13 @@
+package com.aws.practise.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class awsManagerPractice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(awsManagerPractice.class, args);
+	}
+
+}
